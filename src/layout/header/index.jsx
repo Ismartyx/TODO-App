9808a23e7bd47f96515951679275524738a49d53
Ismartyx <Saeedojari@gmail.com>
2022-08-21 17:39:27 +0430

    chore: remove clear complete button

--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -1,17 +1,10 @@
 import Searchbar from "./components/searchbar/searchbar";
 import styles from "./styles.module.css";
 
-const Header = ({ handleFilter, setSearchTerm }) => {
+const Header = ({ setSearchTerm }) => {
   return (
     <header className={styles.header}>
-      <div className="flex">
-        <button className="ml-5" onClick={handleFilter}>
-          Clear Completed
-        </button>
-      </div>
-      <h1 className="text-3xl sans-serif text-sky-700 font-medium">
-        To-Do List
-      </h1>
+      <h1 className="ml-2 text-xl sans-serif text-slate-50 font-bold">ToDo</h1>
       <div className={styles.searchAdd}>
         <Searchbar setSearchTerm={setSearchTerm} />
       </div>
