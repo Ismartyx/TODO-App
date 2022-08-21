6a2082c020c569341ed69d6ea0162e3cddce3d03
Ismartyx <Saeedojari@gmail.com>
2022-08-21 17:32:08 +0430

    feat: add customize scale value

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        15: "0.15",
+      },
+    },
   },
   plugins: [],
 };
