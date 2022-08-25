f58513352e1a367acee9c5edbfcd7565f24e0033
Ismartyx <Saeedojari@gmail.com>
2022-08-25 11:10:01 +0430

    fix: fix unnecessary re-renderings

--- src/App.js
+++ src/App.js
@@ -4,42 +4,46 @@ import ToDoList from "./layout/hero";
 import ToDoForm from "./layout/hero/components/toDoForm";
 import { useLocalStorageState } from "./layout/hero/components/LocalStorageState";
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 function App() {
   const [toDoList, setToDoList] = useLocalStorageState("tasks", []);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleToggle = (id) => {
-    let mapped = toDoList.map((task) => {
-      return task.id === id
-        ? { ...task, complete: !task.complete }
-        : { ...task };
-    });
-    setToDoList(mapped);
-  };
+  const handleToggle = useCallback(
+    (id) => {
+      let mapped = toDoList.map((task) => {
+        return task.id === id
+          ? { ...task, complete: !task.complete }
+          : { ...task };
+      });
+      setToDoList(mapped);
+    },
+    [setToDoList, toDoList]
+  );
 
-  const removeToDos = (id) => {
-    const filterTodo = toDoList.filter((task) => {
-      return task.id !== id;
-    });
-    setToDoList(filterTodo);
-  };
+  const removeToDos = useCallback(
+    (id) => {
+      const filterTodo = toDoList.filter((task) => {
+        return task.id !== id;
+      });
+      setToDoList(filterTodo);
+    },
+    [setToDoList, toDoList]
+  );
 
-  const handleFilter = () => {
+  const handleFilter = useCallback(() => {
     let filtered = toDoList.filter((task) => {
       return !task.complete;
     });
     setToDoList(filtered);
-  };
+  }, [setToDoList, toDoList]);
 
   const addTask = (userInput, desc) => {
-    let copy = [...toDoList];
-    copy = [
-      ...copy,
+    setToDoList((previousState) => [
+      ...previousState,
       { id: nanoid(), task: userInput, description: desc, complete: false },
-    ];
-    setToDoList(copy);
+    ]);
   };
 
   return (
